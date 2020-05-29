b61d61a828d604163e9bfa72565a19bf56b37286
knandan15 <48648230+knandan15@users.noreply.github.com>
2020-05-29 18:12:02 +0530

    Update 2D ArrayDS.cpp

--- 2D ArrayDS.cpp
+++ 2D ArrayDS.cpp
@@ -1,1 +1,17 @@
 //https://www.hackerrank.com/challenges/2d-array/problem
+
+int hourglassSum(vector<vector<int>> arr) {
+    int sum,result=INT_MIN;
+    for(int r=0;r<=3;r++)
+    {
+        for(int c=0;c<=3;c++)
+        {
+            sum= arr[r][c]+arr[r][c+1]+arr[r][c+2]+arr[r+1][c+1]+arr[r+2][c]+arr[r+2][c+1]+arr[r+2][c+2];
+        
+        result=std::max(result,sum);
+        }
+    }
+    return result;
+
+
+}
